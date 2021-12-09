52fa2432b5e18825f53253b1b7da0f1ee7f967b6
seRon <slikeliran@gmail.com>
2021-12-09 11:58:58 +0200

    App setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { hot } from "react-hot-loader";
+
+const App = () => {
+	return (
+		<div>
+			<h1>Hello Mars!!!!</h1>
+		</div>
+	);
+};
+
+export default hot(module)(App);
