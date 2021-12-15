a77ff1f0416dff285f9c8333192f2d322bd10eef
seRon <slikeliran@gmail.com>
2021-12-15 13:13:17 +0200

    Replaced css with styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
 import TodoList from "./todos/TodoList";
 import { hot } from "react-hot-loader";
 
+const AppContainer = styled.div`
+	margin: 1rem;
+	font-family: Arial, Helvetica, sans-serif;
+	color: #222222;
+	width: 100vw;
+	height: 100vh;
+`;
+
 const App = () => {
 	return (
-		<div>
+		<AppContainer>
 			<TodoList />
-		</div>
+		</AppContainer>
 	);
 };
 
